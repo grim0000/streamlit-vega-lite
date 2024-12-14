f1c2e0f3d5d7eb85df62b7e9b365789899473206
Sunil <sunillal360@gmail.com>
2024-12-14 09:51:24 +0530

    first

---
+++ install.sh
@@ -0,0 +1,8 @@
+# install.sh
+# 2. Install required packages
+pip install streamlit yfinance hashlib importlib pandas numpy scikit-learn
+
+# 3. Run the Streamlit app
+streamlit run app.py
+
+# 4. Note: The app will run on a free available local port (default is 8501)
